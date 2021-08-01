814f18cca6427f82caeb67b73d1e30b7df9ed9a4
TBMath <4079570.Tirtha@gmail.com>
2021-08-01 22:40:38 +1000

    add

--- app3.py
+++ app3.py
@@ -1,2 +1,10 @@
-#print hi
-# print randomize
+from pprint import pprint
+sentence = "This is a common question"
+char_dict = {}
+for char in sentence:
+    if char in char_dict:
+        char_dict[char] += 1
+    else:
+        char_dict[char] = 1
+sort = sorted(char_dict.items(), key=lambda kv: kv[1], reverse=True)
+pprint(sort)
