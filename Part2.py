dfb585c218d8e18f07a0d3a39a9a4572b328dd4b
TBMath <4079570.Tirtha@gmail.com>
2021-08-06 08:42:03 +1000

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -1,3 +1,11 @@
-def hi(nus):
-    return "hi"
-print(hi("h"))
+def fizz_buzz(input):
+    if input % 5 or 3 == 0:
+        fb = "FizzBuzz"
+    if input % 3 == 0:
+        fb = "Fizz"
+    elif input % 5 == 0:
+        fb = "Buzz"
+    else:
+        fb = f"{input}"
+    return fb
+    print(fizz_buzz(5))
