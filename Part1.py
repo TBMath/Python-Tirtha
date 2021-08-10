4de4e0cefb1a054aeb391890e1bd2d670476a643
TBMath <4079570.Tirtha@gmail.com>
2021-08-10 23:02:00 +1000

    11:01 10/08/2021
    Co-Authored-By: Tirtha Biswas <tirtha.biswas@education.nsw.gov.au>

--- Part1.py
+++ Part1.py
@@ -1,8 +1,7 @@
-from pprint import pprint
 products = [
     ("Product1", 10),
     ("Product2", 8),
     ("Product3", 12),
 ]
-sorted_products = [1 for x in products]
-pprint(sorted_products)
+sorted_products = sorted(products, key=lambda item: item[1])
+print(sorted_products)
--- app1.py
+++ app1.py
@@ -1,2 +1,4 @@
-message = "Hello"
-print({tuple(message)})
+dictionary_1 = [("Tirtha", 10),("Mukta", 38), ("Srimaa",2),("Kumar",40)]
+print("Oldest to Youngest")
+sorted_products = sorted(dictionary_1, key=lambda item: item[1], reverse=True)
+print(sorted_products)
