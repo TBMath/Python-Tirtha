746af6c119105da2eb57af8d5acf6905bb9e5569
TBMath <4079570.Tirtha@gmail.com>
2021-08-12 14:48:36 +1000

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,4 +1,6 @@
 dictionary_1 = [("Tirtha", 10),("Mukta", 38), ("Srimaa",2),("Kumar",40)]
 print("Oldest to Youngest")
 sorted_products = sorted(dictionary_1, key=lambda item: item[1], reverse=True)
-print(sorted_products)
+def pope(input):
+    return sorted_products.pop(lambda nums: nums[1])
+print(pope("hi"))
