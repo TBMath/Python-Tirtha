54541775ec0b62ee3e23b92ab5c9800b2823f5a6
TBMath <4079570.Tirtha@gmail.com>
2021-08-27 22:04:22 +1000

    Commit

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
+from os import write
 from pathlib import Path
-path = Path("Python")
-for p in path:
-    print(path.iterdir())
+from zipfile import PyZipFile, ZipFile
+with ZipFile("emergancy.zip", "w") as file:
+    for p in Path(r"C:\Users\Tirtha Biswas\Documents\GitHub\Python").rglob("*.*"):
+        file.write(p)
+
+
+
 
